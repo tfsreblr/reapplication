bf1dba221c3b6133b502b389e46e1473d63425b3
tfsreblr/sampleapp <satish.reddy@thermofisher.com>
2016-08-08 00:12:16 +0530

    Updated print commands

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,7 +3,6 @@ public class JavaHelloWorld
 	public static void main (String [] args)
 	{
 		System.out.println("Java Hello World1");
-		System.out.println("Java Hello World2");
 		System.out.println("Hello RE Team");
 	}
 }
