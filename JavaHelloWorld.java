1ffc5e0211548e5add9f374e5788c1b38e51e6b5
tfsreblr/sampleapp <satish.reddy@thermofisher.com>
2016-08-07 22:03:03 +0530

    First version of code

---
+++ JavaHelloWorld.java
@@ -0,0 +1,9 @@
+public class JavaHelloWorld
+{
+	public static void main (String [] args)
+	{
+		System.out.println("Java Hello World1");
+		System.out.println("Hello Again");
+		System.out.println("Hello 2");
+	}
+}
