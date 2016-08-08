c1d3b0d35f5b6163de19b614cf8f20e66688064a
tfsreblr/sampleapp <satish.reddy@thermofisher.com>
2016-08-08 23:12:13 +0530

    Updated message v1.0.5

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,7 +2,7 @@ public class JavaHelloWorld
 {
 	public static void main (String [] args)
 	{
-		System.out.println("Java Hello World1");
+		System.out.println("Welcome to Dockers Demo ");
 		System.out.println("Hello RE Team");
 	}
 }
