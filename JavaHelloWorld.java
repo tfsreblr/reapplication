5e1358968a9d5536afa199dfc0be06507a629c9f
tfsreblr/sampleapp <satish.reddy@thermofisher.com>
2016-08-08 23:32:21 +0530

    Thanks for watching demo

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,6 +3,7 @@ public class JavaHelloWorld
 	public static void main (String [] args)
 	{
 		System.out.println("Welcome to Dockers Demo ");
+		System.out.println("Thanks for watching Demo");
 		System.out.println("Hello RE Team");
 	}
 }
